d340be6b1265a287ab4e10f635e40c15120d3e16
Gary McCann <g.r.mccann@googlemail.com>
2020-04-17 23:53:20 +0100

    Lookup and print stock price

---
+++ README.md
@@ -0,0 +1,9 @@
+## CLI Stock Checker
+
+Little node utility to get the current stock price on the command line
+
+You will need to grab an API_KEY from [https://www.alphavantage.co]
+
+Install globally by running
+
+`npm -i -g .`
--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("stock");
+const fetch = require("node-fetch");
+const chalk = require("chalk");
+
+const symbol = "TSLA";
+const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${
+  process.env.API_KEY
+}`;
+
+const getData = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const ticker = async () => {
+  const data = await getData(url);
+  const properties = data["Global Quote"];
+
+  // for (let [key, value] of Object.entries(properties)) {
+  //   console.log(`${key}: ${value}`);
+  // }
+
+  const price = `$${properties["05. price"]}`;
+  const change = properties["09. change"];
+  console.log(
+    `Stock price for ${chalk.blue(symbol)} is ${
+      change > 0 ? chalk.green(price) : chalk.red(price)
+    }`
+  );
+};
+
+ticker();
